c9e34da27424cf840bc5f0c6b38b53c056760845
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-03-29 15:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function data(){
+	setTimeout(()=>{
+		var age = document.getElementById("age").value;
+	var name = document.getElementById("name").value;
+	if(age>=18){
+		window.alert(`Welcome, ${name}. You can vote.`)
+	}else{
+		window.alert(`Oh sorry ${name}. You aren't old enough.`)
+	}
+	},4000)
+}
